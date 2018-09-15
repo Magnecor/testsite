cc9ce27286a217b9c68160b26c409c8e3e5e60d4
“magnecor” <info@develop.ru>
2018-09-15 11:00:13 -0700

    admin add games

--- resources/views/admin/adminpanel.blade.php
+++ resources/views/admin/adminpanel.blade.php
@@ -1,5 +1,31 @@
-@extends('layouts.app')
+@extends('admin.layouts.app_admin')
 
 @section('content')
-<h1>Admin panel</h1>
+
+<div class="container">
+  <form>
+    <div class="form-row">
+      <div class="col-md-4 mb-3">
+
+        <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Название игры" value="" required>
+        <div class="valid-feedback">
+          Заполнено верно!
+        </div>
+      </div>
+
+      <div class="col-md-4 mb-3">
+
+     <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Сторона-фракция" value="">
+     <div class="valid-feedback">
+       Заполнено верно!
+     </div>
+   </div>
+   <div class="col-md-4 mb-3">
+
+  <button class="btn btn-primary align-middle" type="submit">Сохранить</button>
+
+
+
+  </form>
+</div>
 @endsection
